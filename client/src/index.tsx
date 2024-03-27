51fc0a88d41a006c944b8187b8b820f9ddcd2abd
mV22110702 <fieldlavender70@gmail.com>
2024-03-27 22:56:10 +0200

    First commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import GlobalStyles from './styles';
-import Pages from './pages';
+import GlobalStyles from './styles'
+import Pages from './pages'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById('root')!)
+const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache(),
+})
 
 root.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <GlobalStyles />
+            <Pages />
+        </ApolloProvider>
+    </React.StrictMode>,
+)
